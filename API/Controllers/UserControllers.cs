9c6da5dcf6adf289935aec7d72c663b25187c3af
Rakshan2000 <ckrakshan11@gmail.com>
2024-07-31 09:33:28 +0530

    Commit_3007

---
+++ API/Controllers/UserControllers.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API;
+
+[ApiController]
+[Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+public class UserControllers(DataContext context) : ControllerBase
+{
+    public DataContext Context { get; }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users = context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUsers(int id)
+    {
+        int id = context.Users.Find(id);
+
+        return id;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetdotncore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors();
+builder.Services.AddDbContext<DbContext>(opt=>{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
